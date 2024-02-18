ea4454f7c1cae51128586d62babd7cc64ec96677
edivinofilho <edivinofilho@hotmail.com>
2024-02-18 18:51:51 -0300

    feat: criação dos bancos de dados

---
+++ README.md
@@ -0,0 +1,8 @@
+Pontos para trabalhar - Projeto 4
+- Várias páginas (4)
+- Animação
+- Design responsivo
+- Barra de Filtragem
+
+Modelo:
+https://dribbble.com/shots/15168425-Finding-Doctor-Landing-Page
---
+++ src/js/treatmentsDatabase.js
@@ -0,0 +1,62 @@
+export const data = [
+  {
+    id: 1,
+    img: "./img/",
+    treatment: "Facial Cleansing",
+    description:
+      "This involves cleansing the skin to remove dirt, oil, and impurities, often using gentle cleansers tailored to different skin types.",
+    fee: 100,
+    city: "London",
+    contact: "555.1234",
+  },
+  {
+    id: 2,
+    img: "./img/",
+    treatment: "Chemical Peels",
+    description:
+      "Chemical peels involve the application of a chemical solution to the skin, which exfoliates the outer layer, revealing smoother, more even-toned skin beneath. They can help with acne, fine lines, and uneven pigmentation.",
+    fee: 100,
+    city: "Oxford",
+    contact: "555.1234",
+  },
+  {
+    id: 3,
+    img: "./img/",
+    treatment: "Botox Injections",
+    description:
+      "Botox is a popular cosmetic treatment that involves injecting a purified form of botulinum toxin into targeted muscles to temporarily reduce the appearance of wrinkles and fine lines, particularly in areas like the forehead and around the eyes.",
+    fee: 100,
+    city: "Liverpool",
+    contact: "555.1234",
+  },
+  {
+    id: 4,
+    img: "./img/",
+    treatment: "Dermal Fillers",
+    description:
+      "Dermal fillers are injectable substances used to restore volume, smooth lines, and enhance facial contours. They can be used to plump lips, fill in wrinkles, and add volume to areas like the cheeks and temples.",
+    fee: 100,
+    city: "Glasgow",
+    contact: "555.1234",
+  },
+  {
+    id: 5,
+    img: "./img/",
+    treatment: "Acne Treatments",
+    description:
+      "Acne treatments encompass a range of options, including topical creams, oral medications, chemical peels, and laser therapy, aimed at reducing inflammation, unclogging pores, and preventing future breakouts.",
+    fee: 100,
+    city: "London",
+    contact: "555.1234",
+  },
+  {
+    id: 6,
+    img: "./img/",
+    treatment: "Microneedling",
+    description:
+      "Microneedling, also known as collagen induction therapy, involves using a device with fine needles to create tiny punctures in the skin's surface, stimulating collagen production and improving the skin's texture, tone, and firmness.",
+    fee: 100,
+    city: "Manchester ",
+    contact: "555.1234",
+  },
+];
