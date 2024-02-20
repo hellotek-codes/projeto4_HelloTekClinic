8b1ccc30b6077308a94b8ae84da9abc350b16eef
edivinofilho <edivinofilho@hotmail.com>
2024-02-20 15:06:08 -0300

    feat: criação da função de busca

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,57 +1,107 @@
 import { data } from "./dataBase/docDatabase.js";
 
 const cardsContainer = document.getElementById("cards_container");
+const searchButton = document.getElementById("search_button");
+const searchInput = document.getElementById("search_input");
 
-data.forEach((doctor) => {
-  const li = document.createElement("li");
-  li.classList.add("cards");
+// Criação dos cards
+function displayCards(data) {
+  data.forEach((doctor) => {
+    const li = document.createElement("li");
+    li.classList.add("cards");
 
-  if (doctor.id % 2 === 0) {
-    li.style.borderLeft = "10px solid #e7649a";
-  } else {
-    li.style.borderLeft = "10px solid #0a81d1";
+    if (doctor.id % 2 === 0) {
+      li.style.borderLeft = "10px solid #e7649a";
+    } else {
+      li.style.borderLeft = "10px solid #0a81d1";
+    }
+
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add("img_container");
+
+    const img = document.createElement("img");
+    img.src = doctor.img;
+    imgContainer.appendChild(img);
+    li.appendChild(imgContainer);
+
+    const docDescription = document.createElement("div");
+    docDescription.classList.add("doc_description");
+
+    const descriptionTitle = document.createElement("div");
+
+    const name = document.createElement("h2");
+    name.textContent = doctor.drName;
+    descriptionTitle.appendChild(name);
+
+    const description = document.createElement("p");
+    description.textContent = doctor.description;
+    descriptionTitle.appendChild(description);
+
+    const city = document.createElement("span");
+    city.textContent = doctor.city;
+    descriptionTitle.appendChild(city);
+
+    docDescription.appendChild(descriptionTitle);
+
+    const descriptionContact = document.createElement("div");
+
+    const consultationFee = document.createElement("p");
+    consultationFee.textContent = `Consulta R$ ${doctor.consultationFee}`;
+    descriptionContact.appendChild(consultationFee);
+
+    const contact = document.createElement("p");
+    contact.textContent = `Contato: ${doctor.contact}`;
+    descriptionContact.appendChild(contact);
+
+    docDescription.appendChild(descriptionContact);
+
+    li.appendChild(docDescription);
+
+    cardsContainer.appendChild(li);
+  });
+}
+
+displayCards(data);
+
+// Mecanismo de procura
+searchButton.addEventListener("click", handleSearch);
+
+function handleSearch(){
+
+  const searchTerm = searchInput.value.trim().toLowerCase();
+  const cards = document.querySelectorAll(".cards");
+  
+  let matchedCards = [];
+
+  for (let i = 0; i < data.length; i++) {
+    let name = data[i].drName.toLowerCase();
+    let description = data[i].description.toLowerCase();
+    let city = data[i].city.toLowerCase();
+
+    if (
+      name.includes(searchTerm) ||
+      description.includes(searchTerm) ||
+      city.includes(searchTerm)
+    ) {
+      matchedCards.push(data[i]);
+    }
   }
 
-  const imgContainer = document.createElement("div");
-  imgContainer.classList.add("img_container");
+  if (matchedCards.length !== 0) {
+    cardsContainer.innerHTML = "";
+    displayCards(matchedCards);
+    clearTextInput()
+  } else {
+    alert("Termo não encontrado, por favor pesquise novamente!");
+    displayCards(data);
+    clearTextInput()
+  }
+};
 
-  const img = document.createElement("img");
-  img.src = doctor.img;
-  imgContainer.appendChild(img);
-  li.appendChild(imgContainer);
-
-  const docDescription = document.createElement("div");
-  docDescription.classList.add("doc_description");
-
-  const descriptionTitle = document.createElement("div");
-
-  const name = document.createElement("h2");
-  name.textContent = doctor.drName;
-  descriptionTitle.appendChild(name);
-
-  const description = document.createElement("p");
-  description.textContent = doctor.description;
-  descriptionTitle.appendChild(description);
-
-  const city = document.createElement("span");
-  city.textContent = doctor.city;
-  descriptionTitle.appendChild(city);
-
-  docDescription.appendChild(descriptionTitle);
-
-  const descriptionContact = document.createElement("div");
-
-  const consultationFee = document.createElement("p");
-  consultationFee.textContent = `Consulta R$ ${doctor.consultationFee}`;
-  descriptionContact.appendChild(consultationFee);
-
-  const contact = document.createElement("p");
-  contact.textContent = `Contato: ${doctor.contact}`;
-  descriptionContact.appendChild(contact);
-
-  docDescription.appendChild(descriptionContact);
-
-  li.appendChild(docDescription);
-
-  cardsContainer.appendChild(li);
-});
+//Limpar a barra de procura
+function clearTextInput(){
+  let textInput = document.querySelector('#search_input');
+  if(textInput != ''){
+      textInput.value = '';
+  }
+};
